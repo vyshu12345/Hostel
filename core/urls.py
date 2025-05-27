1ccc52f62042856c36249a5dcbf2973528c428d1
vyshu12345 <125803329+vyshu12345@users.noreply.github.com>
2025-05-27 18:43:33 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hostels.urls')),  # includes all hostel app urls here
+    path('', include('hostels.urls')), 
 ]
 
 if settings.DEBUG:
